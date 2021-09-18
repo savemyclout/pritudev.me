55d7a6ad408a4184ed7eef2b41065dd34fbb93db
pritudev <contact.pritulx@gmail.com>
2021-09-18 10:44:18 +0530

    Updated Color palette!

--- src/components/layout/About.js
+++ src/components/layout/About.js
@@ -13,7 +13,7 @@ const About = () => {
       </section>
       <section>
         <div className="about_text">
-          <h2 className="about_header">🎙️ Your host</h2>
+          <h2 className="about_header">👑 Developer</h2>
           <p className="about_body">
             Hi, I'm <span className="hi-code">Prit</span>. I've been building
             websites for over five years. I'm 16 years old web developer and
