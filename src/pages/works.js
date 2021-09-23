a85a2172f568fc13d427484b65119896db02b924
pritudev <contact.pritulx@gmail.com>
2021-09-23 10:58:23 +0530

    Fixed Errorss

--- src/pages/works.js
+++ src/pages/works.js
@@ -14,6 +14,7 @@ import vsc from './svgs/vsc.svg'
 import css from './svgs/css.svg'
 import { Link } from 'gatsby'
 import Projects from '../components/layout/Projects'
+import MetaTags from 'react-meta-tags'
 
 export default () => {
   return (
