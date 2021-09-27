7eae4c71e49f64c53d023afdfbf52090ec94c633
pritudev <contact.pritulx@gmail.com>
2021-09-27 10:54:37 +0530

    New 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -2,8 +2,9 @@ import React from 'react'
 import Footer from '../components/layout/Footer.js'
 import Layout from '../components/layout/Layout.js'
 import MetaTags from 'react-meta-tags'
+import '../theme/main.scss'
 
-export default () => {
+const notFound = () => {
   return (
     <Layout>
       <MetaTags>
@@ -11,9 +12,17 @@ export default () => {
         <meta name="description" content="Error 404 Page Not Found" />
         <meta property="og:title" content="404 Page Not Found" />
       </MetaTags>
-      <title>Error Page Not Found</title>
-      <h2 style={{ textAlign: 'center' }}>Page Not Found</h2>
-      <Footer />
+      <h2 id="error__heading" style={{ textAlign: 'center' }}>
+        Page Not Found
+      </h2>
+      <h5 id="error__body">
+        Sorry 😔—we couldn’t find what you<br></br>were looking for.
+      </h5>
+      <h5 id="error__link">
+        <a href="/">Go back to home page</a>
+      </h5>
     </Layout>
   )
 }
+
+export default notFound
