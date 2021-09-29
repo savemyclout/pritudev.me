1851bc6779ce00ff5a249e973884b12d7bc9e66d
pritudev <contact.pritulx@gmail.com>
2021-09-29 09:41:08 +0530

    🔨 chore: remove emojis

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -14,7 +14,7 @@ const Contact = () => {
         <meta property="og:title" content="Contact" />
       </MetaTags>
       <div className="container">
-        <h2 style={{ fontFamily: 'cubano', paddingTop: '12px' }}>Contact 📝</h2>
+        <h2 style={{ fontFamily: 'cubano', paddingTop: '12px' }}>Contact</h2>
         <hr id="hr-body" />
 
         <h4 style={{ textAlign: 'center', fontFamily: 'sofiapro' }}>
