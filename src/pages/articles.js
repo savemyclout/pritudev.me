8c9a2d7a2644b885e11d70f5c6458648ed9f337c
pritudev <contact.pritulx@gmail.com>
2021-10-06 07:38:43 +0530

    chore: update meta tag

--- src/pages/articles.js
+++ src/pages/articles.js
@@ -21,7 +21,7 @@ const Posts = () => {
         />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <meta name="author" content="Prit Dev" />
-        <meta property="og:title" content="Pritudev" />
+        <meta property="og:title" content="Articles Post" />
       </Helmet>
       <div id="heading">
         <h2
