29f4cb716fcb09c5929709239dbc23bee33ecea6
pritudev <contact.pritulx@gmail.com>
2021-10-24 10:17:15 +0530

    style: update buttons and make projects tab responsive

--- src/components/layout/Showcase.js
+++ src/components/layout/Showcase.js
@@ -7,6 +7,7 @@ import Layout from './Layout.js'
 import About from './About.js'
 import githubCat from './svgs/github-cat.svg'
 import mentor from './svgs/mentor.svg'
+import githubCatPixel from './svgs/github-cat-pixel.png'
 
 const Showcase = ({ data }) => {
   const [followers, setFollowers] = useState(0)
@@ -49,15 +50,15 @@ const Showcase = ({ data }) => {
                 target="_blank"
               >
                 <button id="indigo-btn" to="https://github.com/pritudev">
-                  <span id="follow-btn">
-                    {' '}
+                  <span>
                     <img
+                      style={{ paddingBottom: '2px' }}
                       height="30px"
                       widht="30px"
-                      src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAACgAAAAoCAYAAACM/rhtAAAAAXNSR0IArs4c6QAAAlFJREFUWEfVmLtKA0EUhg1iETD2ETtJbATFlHaxtLFSMAHB0sZOsPLSCGkkCj5BougrCNamWBMrQ6JdRDshEawk4pn9zcwwl911i9k0k90zc+afb8+cuaTG9L+hb0oZ6sRhMvZj6txZgSSsXNokOrX6JShhMBAelZ7gx9AP+VcRTJbAp3abRuJ5D1RixMAHe1Ccsh/ZvwzNStAlgcKnDUsmKEFdvUH/k0ydbkeIeZ5gMgViZP8lhPaZqUmlq8gEEyOQi41YYBYKS+RHBhA6BhXo3RKIvPV1dxSLMDhJFw/or0wyNEFnBWIN7jevBXKZxXXhedC6MZLV1Z/4YCuUTBJAsKJo82DiBILE7doyjfzq+Z3Kau1ESdBWP3aCtg5llbb6EQSO+0vdBvVVq7PY6zfZfhAdbs/PClpsBHX1RwKP/dm8QKXnPfr+vyn8uBhMuEBbErw/rAaKTZAb5cOYCDogEBLMn9omVGcPSw5+FDtq9wRqjpfhhO6W9wV4F+dbSpjpIou5fI5lg073RZi1cqNfgokRqPvsNIB8Li+NOFo0jsiJZw+dN56gmwKzjSERWjkrkUDuJsGPFUYu7p+879MSdF4glEPo9E6BXmFfhthDPZzKdIepoHZ532eKQbI5L5AbAcVktsGyT6Y8589eNutwN4ObB/lQFdZuI6m9m3FZIGBK+e8vb5EdpzEFAWpns9vuBXU5j49VtwRWeiwf7s2ob4UrPRaTMdiNN7VcP+KO2lmBEIZve/oqZqS3FnuurLISdrxH7bB2tJP9yPkw5brAHy6QY3VmdklZAAAAAElFTkSuQmCC"
-                    />{' '}
-                    {Number(followers)}
+                      src={githubCatPixel}
+                    />
                   </span>{' '}
+                  <span id="follow-btn">{Number(followers)}</span>{' '}
                   <span id="followers-num">GitHub followers</span>
                 </button>
               </Link>
